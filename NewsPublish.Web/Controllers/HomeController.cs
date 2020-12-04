b75462a9c8ccc81b3216a4c594543a5ae92ba292
DESKTOP-I0L625T\jy <hong3979@163.com>
2020-12-04 11:33:46 +0800

    添加遗漏的文件

---
+++ NewsPublish.Web/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NewsPublish.Model.Response;
+using NewsPublish.Service;
+using NewsPublish.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPublish.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private NewsService _newsService;
+        private BannerService _bannerService;
+        public HomeController(NewsService newsService, BannerService bannerService)
+        {
+            this._newsService = newsService;
+            this._bannerService = bannerService;
+        }
+
+        public ActionResult Index()
+        {
+            ViewData["Title"] = "首页";
+            return View(_newsService.GetNewsClassifyList());
+        }
+        [HttpGet]
+        public JsonResult GetBanner()
+        {
+            return Json(_bannerService.GetBannerList());
+        }
+        [HttpGet]
+        public JsonResult GetTotalNews()
+        {
+            return Json(_newsService.GetNewsCount(c => true));
+        }
+        [HttpGet]
+        public JsonResult GetHomeNews()
+        {
+            return Json(_newsService.GetNewsList(c => true, 6));
+        }
+        [HttpGet]
+        public JsonResult GetNewCommentNewsList()
+        {
+            return Json(_newsService.GetNewCommentNewsList(c => true, 5));
+        }
+        [HttpGet]
+        public JsonResult SearchOneNews(string keyword)
+        {
+            if (string.IsNullOrEmpty(keyword))
+                return Json(new ResponseModel { code = 0, result = "关键字不能为空" });
+            return Json(_newsService.GetSearchOneNews(c => c.Title.Contains(keyword)));
+        }
+        public ActionResult Wrong()
+        {
+            ViewData["Title"] = "404";
+            return View(_newsService.GetNewsClassifyList());
+        }
+    }
+}
---
+++ NewsPublish.Web/Controllers/NewsController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsPublish.Model.Request;
+using NewsPublish.Model.Response;
+using NewsPublish.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPublish.Web.Controllers
+{
+    public class NewsController : Controller
+    {
+        private NewsService _newsService;
+        private CommentService _commentService;
+        public NewsController(NewsService newsService, CommentService commentService)
+        {
+            _newsService = newsService;
+            _commentService = commentService;
+        }
+        public IActionResult Classify(int id)
+        {
+            if (id <= 0)
+                Response.Redirect("/Home/Index");
+            var classify = _newsService.GetOneNewsClassify(id);
+            ViewData["ClassifyName"] = "首页";
+            ViewData["NewsList"] = new ResponseModel();
+            ViewData["NewCommentNews"] = new ResponseModel();
+            ViewData["Title"] = "首页";
+            if (classify.code == 0)
+            {
+                Response.Redirect("/Home/Index");
+            }
+            else
+            {
+                ViewData["ClassifyName"] = classify.data.Name;
+                var newsList = _newsService.GetNewsList(c => c.NewsClassifyId == id, 6);
+                ViewData["NewsList"] = newsList;
+                var newCommentNews = _newsService.GetNewCommentNewsList(c => c.NewsClassifyId == id, 5);
+                ViewData["NewCommentNews"] = newCommentNews;
+                ViewData["Title"] = classify.data.Name;
+            }
+            return View(_newsService.GetNewsClassifyList());
+        }
+        public IActionResult Detail(int id)
+        {
+            ViewData["Title"] = "详情页";
+            if (id < 0)
+                Response.Redirect("/Home/Index");
+            var news = _newsService.GetoneNews(id);
+            ViewData["News"] = new ResponseModel();
+            ViewData["RecommendNews"] = new ResponseModel();
+            ViewData["Comments"] = new ResponseModel();
+            if (news.code == 0)
+            {
+                Response.Redirect("/Home/Index");
+            }
+            else
+            {
+                ViewData["Title"] = news.data.Title + "-" + "详情页";
+                ViewData["News"] = news;
+                var recommendNews = _newsService.GetRecommendNewsList(id);
+                ViewData["RecommendNews"] = recommendNews;
+                var comments = _commentService.GetCommentList(c => c.NewsId == id);
+                ViewData["Comments"] = comments;
+            }
+            return View(_newsService.GetNewsClassifyList());
+        }
+        [HttpPost]
+        public JsonResult AddComment(AddComment comment)
+        {
+            if (comment.NewsId <= 0)
+                return Json(new ResponseModel { code = 0, result = "新闻不存在" });
+            if (string.IsNullOrEmpty(comment.Contents))
+                return Json(new ResponseModel { code = 0, result = "内容不能为空" });
+            return Json(_commentService.AddComment(comment));
+        }
+    }
+}
